6acee544bf87f4c602ff860c8f83509ba17ccd54
KeshavThosar <keshavjaythosar@gmail.com>
2024-07-06 12:44:45 +0530

    Updated StoreObject model

--- app/database/models.py
+++ app/database/models.py
@@ -14,18 +14,20 @@ def get_db():
 
 # Model Declarations
 class User(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   email = db.Column(db.String(120), unique=True, nullable=False)
-   first_name = db.Column(db.String(50), nullable=False)
-   last_name = db.Column(db.String(50), nullable=False)
-   password_hash = db.Column(db.String(256), nullable=False)
-   store_objects = db.relationship('StoreObject', backref='user', lazy=True)
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  first_name = db.Column(db.String(50), nullable=False)
+  last_name = db.Column(db.String(50), nullable=False)
+  password_hash = db.Column(db.String(256), nullable=False)
+  store_objects = db.relationship('StoreObject', backref='user', lazy=True)
 
 
 class StoreObject(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    file_hash = db.Column(db.String(256), unique=True, nullable=False)
-    creation_datetime = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  file_name = db.Column(db.String(256), unique=True, nullable=False)
+  file_hash = db.Column(db.String(256), nullable=False)
+  file_identifier = db.Column(db.String(36), nullable=False)
+  creation_datetime = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
