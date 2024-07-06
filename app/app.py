574d55f196d5709c6a6d29db5a5b9109febde3e9
KeshavThosar <keshavjaythosar@gmail.com>
2024-07-06 12:42:02 +0530

    Added the storage api

--- app/app.py
+++ app/app.py
@@ -1,19 +1,23 @@
+'''
+The main file that runs the server for flask 
+'''
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from database import setup_db
+from api.storage import Storage
 
 app = Flask(__name__)
 db = setup_db(app)
 
 with app.app_context():
-    db.create_all()
-
-@app.route('/')
-def home():
-    return "Hello, World!"
+  db.create_all()
+  
+storage = Storage(db)
+app.add_url_rule('/storage/<endpoint>', 'storage', storage.handle_endpoint, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+  app.run(port=5000, debug=True)
 
 
 
