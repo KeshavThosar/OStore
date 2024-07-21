948f8137f49cd3a369db58374759a04c1d8097a3
KeshavThosar <keshavjaythosar@gmail.com>
2024-07-21 18:26:22 +0530

    Integrated login and files dashboard

--- app/static/app.js
+++ app/static/app.js
@@ -19,19 +19,19 @@ const loginPasswordInput = gbid('password')
 window.onload = async () => {
   await init()
   console.log('App loaded')
-} 
+}
 
 document.addEventListener('list-files', list_files)
 
 async function init() {
   const ACCESS_TOKEN = accessTokenInput.value
-  if(ACCESS_TOKEN.length == 0){
+  if (ACCESS_TOKEN.length == 0) {
     try {
       await list_files()
     } catch (error) {
-      
+
     }
-  }else{
+  } else {
     hide_login_form()
   }
   console.log('Access Token: ', ACCESS_TOKEN)
@@ -39,32 +39,32 @@ async function init() {
 }
 
 gbid('login-form').addEventListener('submit', process_form)
-async function process_form(event){
+async function process_form(event) {
   event.preventDefault()
   let loginFormRole = loginFormHeading.dataset['role']
   let username = loginUsernameInput.value
   let password = loginPasswordInput.value
 
-  if(loginFormRole == 'login'){
+  if (loginFormRole == 'login') {
     await login(username, password)
-  }else if(loginFormRole == 'register'){
+  } else if (loginFormRole == 'register') {
     await register(username, password)
   }
 
 }
 
-async function login(email, password){
+async function login(email, password) {
   let req = await fetch('/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: `email=${email}&password=${password}`
-  })  
+  })
   let body = await req.json()
-  if(req.status != 200){
+  if (req.status != 200) {
     popup('Invalid username/password')
-  }else{
+  } else {
     accessTokenInput.value = body['access_token']
     AUTH_HEADER = 'Bearer ' + accessTokenInput.value
     hide_login_form()
@@ -79,22 +79,22 @@ async function register(email, password) {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: `email=${email}&password=${password}`
-  }) 
-  
-  if(req.status == 409){
+  })
+
+  if (req.status == 409) {
     popup('User already exists. Try Log In?')
-  }else{
+  } else {
     await login(email, password)
   }
 }
 
-async function list_files(){
+async function list_files() {
   let req = await fetch('/storage/list', {
     headers: {
       authorization: AUTH_HEADER
     }
   })
-  
+
   let items = await req.json()
   console.log(items)
   let content = `
@@ -109,15 +109,15 @@ async function list_files(){
   </thead>
   <tbody>`
 
-  for(let i = 0; i < items.length; i++){
-    let {created_on, hash, identifier, name} = items[i]
+  for (let i = 0; i < items.length; i++) {
+    let { created_on, hash, identifier, name } = items[i]
     content += `
     <tr>
       <td>${name}</td>
       <td>${created_on}</td>
       <td>${hash}</td>
       <td>`
-    
+
     content += `
     <button class="btn btn-success" onclick="download_file('${identifier}', '${name}')">
       <i class="bi bi-download"></i>
@@ -135,73 +135,93 @@ async function list_files(){
   listFilesDiv.innerHTML = content
 }
 
-async function upload_file(){
-
-}
-
-async function download_file(identifier, filename){
-  popup('Download Started for '+filename)
-  let res = await fetch('/storage/download?id='+identifier, {
-    headers: {authorization: AUTH_HEADER}
+async function download_file(identifier, filename) {
+  popup('Download Started for ' + filename)
+  let res = await fetch('/storage/download?id=' + identifier, {
+    headers: { authorization: AUTH_HEADER }
   })
   let blob = await res.blob()
-  let blobUrl =  URL.createObjectURL(blob)
-  
+  let blobUrl = URL.createObjectURL(blob)
+
   const anchor = document.createElement('a')
   anchor.href = blobUrl
   anchor.download = filename
-  
+
   document.body.appendChild(anchor)
   anchor.click()
-  document.body.removeChild(anchor) 
-  
-  URL.revokeObjectURL(blobUrl) 
+  document.body.removeChild(anchor)
+
+  URL.revokeObjectURL(blobUrl)
 }
 
-async function delete_file(identifier, filename){
+async function delete_file(identifier, filename) {
   cnf_delete = confirm('Delete ' + filename + '?')
-  if(cnf_delete == true){
-    let req = await fetch('/storage/remove?id='+identifier, {
+  if (cnf_delete == true) {
+    let req = await fetch('/storage/remove?id=' + identifier, {
       method: 'DELETE',
-      headers: {authorization: AUTH_HEADER}
+      headers: { authorization: AUTH_HEADER }
     })
 
-    if(req.ok){
-      popup(filename+' deleted successfully')
+    if (req.ok) {
+      popup(filename + ' deleted successfully')
     }
-    
+
     dispatch_list_files()
   }
 
 }
 
-function dispatch_list_files(){
+function dispatch_list_files() {
   document.dispatchEvent(LIST_FILES_EVENT)
 }
 
-function popup(message){
+function popup(message) {
   alertMessageText.innerHTML = message
   alertMessageDiv.classList.remove('d-none')
   setTimeout(() => alertMessageDiv.classList.add('d-none'), 2000)
 }
 
-function hide_login_form(){
+function hide_login_form() {
   // Hides login form, shows dashboard
   gbid('user-auth').classList.add('d-none')
   gbid('files-dashboard').classList.remove('d-none')
 }
 
-function show(type){
-  if(type == 'login'){
+function show(type) {
+  if (type == 'login') {
     gbid('alt-login-message').classList.add('d-none')
     gbid('alt-register-message').classList.remove('d-none')
     loginFormHeading.innerText = 'Login'
     loginFormHeading.dataset['role'] = 'login'
-  }else if(type == 'register'){
+  } else if (type == 'register') {
     gbid('alt-login-message').classList.remove('d-none')
     gbid('alt-register-message').classList.add('d-none')
     loginFormHeading.innerText = 'Register'
     loginFormHeading.dataset['role'] = 'register'
 
   }
+}
+
+gbid('uploadForm').addEventListener('submit', process_upload_form)
+async function process_upload_form(event) {
+  event.preventDefault()
+
+  const fileInput = gbid('fileInput')
+  const file = fileInput.files[0];
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('filename', file.name);
+  popup('Upload in progress')
+  let req = await fetch('/storage/add', { 
+    method: 'POST', 
+    headers: { authorization: AUTH_HEADER }, 
+    body: formData 
+  })
+  let body = await req.json()
+  if(req.ok){
+    dispatch_list_files()
+  }
+  popup(body.message)
+
+
 }
